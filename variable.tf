d5719c21cabef55e94b199275171d1c297a1c04c
Sami Islam <islam.samipkr@gmail.com>
2025-02-07 18:18:01 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,3 +11,12 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
+
+variable "my_variable" {
+  description = "A simple variable"
+  type        = string
+  default     = "Hello, World!"
+}
+output "my_variable_output" {
+  value = var.my_variable
+}
