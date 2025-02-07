e423e6ac6f93b3a56d04c82d6eed6168e5354df7
Sami Islam <islam.samipkr@gmail.com>
2025-02-07 18:09:29 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -9,3 +9,26 @@ resource "azurerm_storage_account" "mcitstorageaccount" {
     environment = "staging"
   }
 }
+variable "storage_account_names" {
+  type    = list(string)
+  default = [
+    "storageaccountname1",
+    "storageaccountname2",
+    "storageaccountname3",
+    "storageaccountname4",
+    "storageaccountname5"
+  ]
+}
+
+resource "azurerm_storage_account" "mcitstorageaccount" {
+  count                    = length(var.storage_account_names)
+  name                     = var.storage_account_names[count.index]
+  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+  location                 = azurerm_resource_group.mcit420zz5um.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
