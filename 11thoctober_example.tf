11b939a41bdcf0b149fb7919c582b462ce61c95a
Sami Islam <islam.samipkr@gmail.com>
2025-02-07 18:28:02 -0500

    Update 11thoctober_example.tf

--- 11thoctober_example.tf
+++ 11thoctober_example.tf
@@ -6,3 +6,41 @@ variable "my_variable" {
 output "my_variable_output" {
   value = var.my_variable
 }
+variable "octobereleven"{
+    type=string
+    default="11"
+}
+variable "octobertwelve"{
+    type=string
+    default="12"
+} 
+output "octobertogether"{
+   value="${var.octobereleven}+${var.octobertwelve}"
+}
+###################################################3
+variable "octoberelevensecondexample1"{
+    type=number
+    default=11
+}
+variable "octoberelevensecondexample2"{
+    type=number
+    default=12
+}
+output "sumofoctoberdates"{
+  value="${var.octoberelevensecondexample1}+${var.octoberelevensecondexample2}"
+}
+output "sumofoctoberdatesreal"{
+  value=var.octoberelevensecondexample1+var.octoberelevensecondexample2
+}
+###########################################################
+variable "mcit_list"{
+    type =list(string)
+    default=["one","two","three"]
+}
+output "mcit_list_output"{
+    value=var.mcit_list
+}
+########################################################
+output "mcit_tranformed_list"{
+    value=[for item in var.mcit_list:"${item}_transformed"]
+}
