617c84eae875b17f117af15e849fd3c59cb96fc9
Sami Islam <islam.samipkr@gmail.com>
2025-02-07 18:25:42 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -12,11 +12,3 @@ variable "tenant_id"{
   type=string
 }
 
-variable "my_mcit_variable" {
-  description = "A simple variable"
-  type        = string
-  default     = "Hello, World!"
-}
-output "my_variable_output2" {
-  value = var.my_mcit_variable
-}
