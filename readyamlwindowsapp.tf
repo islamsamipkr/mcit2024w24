4b754ab914e787ea88d7f2da5a2ef9ab7340c8ab
Sami Islam <islam.samipkr@gmail.com>
2025-05-21 20:02:25 -0400

    Update readyamlwindowsapp.tf

--- readyamlwindowsapp.tf
+++ readyamlwindowsapp.tf
@@ -10,23 +10,16 @@ locals{
         sku_name=windowsapps.sku_name     
       }
     ]
-])
- /*   waf_policy=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
-    waf_policy_list = flatten([
-    for policy in local.waf_policy : [
-      for policies in try(policy.listofwafpolicies, []) :{
-        name=policies.name
-        custom_rules=policies.custom_rules
-        managed_rules=policies.managed_rules
-        }
-    ]
-])*/
+])}
+resource "azurerm_resource_group" "mcitdevrm" {
+  name     = "mcitwindowsapp"
+  location = "canadacentral"
 }
 resource "azurerm_service_plan" "mcitwindowsserviceplan" {
   for_each            ={for sp in local.windows_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_resource_group.mcitdevrm.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
@@ -34,8 +27,8 @@ resource "azurerm_service_plan" "mcitwindowsserviceplan" {
 resource "azurerm_windows_web_app" "mcitwindowswebapp" {
   for_each            = azurerm_service_plan.mcitwindowsserviceplan
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_resource_group.mcitdevrm.location
   service_plan_id     = each.value.id
 
   site_config {}
