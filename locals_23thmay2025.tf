0792a8b551e41b516c1aec5ed65e4caaac3e99fb
Sami Islam <islam.samipkr@gmail.com>
2025-05-23 19:27:20 -0400

    Update locals_23thmay2025.tf

--- locals_23thmay2025.tf
+++ locals_23thmay2025.tf
@@ -1,24 +1,24 @@
 locals {
   node_pools = {
-    internal = {
+    prod = {
       vm_size    = "Standard_DS2_v2"
       node_count = 1
       tags = {
         Environment = "prod"
       }
     }
-    analytics = {
+    dev = {
       vm_size    = "Standard_D4_v3"
       node_count = 2
       tags = {
         Environment = "dev"
       }
     }
-    staging = {
+    qa = {
       vm_size    = "Standard_B2s"
       node_count = 1
       tags = {
-        Environment = "stage"
+        Environment = "qa"
       }
     }
   }
