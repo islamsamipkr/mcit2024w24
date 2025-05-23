afc88259b493a618154df5765845a441904c08b0
Sami Islam <islam.samipkr@gmail.com>
2025-05-23 18:58:36 -0400

    Update mcitkubernetescluster.tf

--- mcitkubernetescluster.tf
+++ mcitkubernetescluster.tf
@@ -20,3 +20,14 @@ resource "azurerm_kubernetes_cluster" "mcitkubecluster" {
     client_secret = var.client_secret
   }
 }
+
+resource "azurerm_kubernetes_cluster_node_pool" "mcitnodepools" {
+  for_each = local.node_pools
+
+  name                  = "mcit-${each.key}"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  vm_size               = each.value.vm_size
+  node_count            = each.value.node_count
+
+  tags = each.value.tags
+}
