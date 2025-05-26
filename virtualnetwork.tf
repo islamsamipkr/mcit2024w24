f7722f3718b192d6057a778c10d1b085e1ab4b1b
Sami Islam <islam.samipkr@gmail.com>
2025-05-26 18:10:16 -0400

    Create virtualnetwork.tf

---
+++ virtualnetwork.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "mcitnetworkrg" {
+  name     = "mcitnetworkrg"
+  location = "East US"
+}
+
+locals {
+  cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+resource "azurerm_virtual_network" "mcitvirtualnetwork" {
+  name                = "mcit-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.mcitnetworkrg.location
+  resource_group_name = azurerm_resource_group.mcitnetworkrg.name
+}
+
+resource "azurerm_subnet" "subnets" {
+  for_each             = { for idx, cidr in local.cidrs : "subnet${idx + 1}" => cidr }
+  name                 = each.key
+  resource_group_name  = azurerm_resource_group.mcitnetworkrg.name
+  virtual_network_name = azurerm_virtual_network.mcitvirtualnetwork.name
+  address_prefixes     =[each.value]
+                         }
