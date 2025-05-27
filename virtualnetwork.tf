5ea174040d3ce95f597db73c5e8746e40a0c2ce4
Sami Islam <islam.samipkr@gmail.com>
2025-05-26 20:23:33 -0400

    Update virtualnetwork.tf

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -16,6 +16,11 @@ resource "azurerm_virtual_network" "mcitvirtualnetwork" {
 
 resource "azurerm_subnet" "subnets" {
   for_each             = { for index, cidr in local.cidrs : "subnet${index + 1}" => cidr }
+/* 
+for 0,"10.0.1.0/24" in ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] :"subnet${0 + 1}"(key) =>"10.0.1.0/24"(value)
+for 1,"10.0.2.0/24" in ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] :"subnet${1 + 1}"(key) =>"10.0.2.0/24"(value)
+for 2,"10.0.3.0/24" in ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] :"subnet${2 + 1}"(key) =>"10.0.3.0/24"(value)
+*/
   name                 = each.key
   resource_group_name  = azurerm_resource_group.mcitnetworkrg.name
   virtual_network_name = azurerm_virtual_network.mcitvirtualnetwork.name
