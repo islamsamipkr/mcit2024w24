9e47f6b394bf35dbc5a84762a2a696105503f77a
Sami Islam <islam.samipkr@gmail.com>
2025-05-28 19:08:30 -0400

    Update subnetread.tf

--- subnetread.tf
+++ subnetread.tf
@@ -22,10 +22,3 @@ resource "azurerm_subnet" "mcitazuresubnet" {
   virtual_network_name = azurerm_virtual_network.mcitvnetmay.name
   address_prefixes     = [each.value]
 }
-output "raw_yaml" {
-  value = file("${path.module}/subnets.yaml")
-}
-
-output "decoded_yaml" {
-  value = yamldecode(file("${path.module}/subnets.yaml"))
-}
