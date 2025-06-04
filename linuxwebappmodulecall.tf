3ebe0951c7fb66ea0781c4beadf8c86411780141
Sami Islam <islam.samipkr@gmail.com>
2025-06-04 19:19:32 -0400

    Update linuxwebappmodulecall.tf

--- linuxwebappmodulecall.tf
+++ linuxwebappmodulecall.tf
@@ -1,8 +1,8 @@
 module "linux_web_app" {
   source              = "./modules/linux_web_app"
   name                = "my-mcit-linux-app"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
 
   service_plan_name = "mymcitlinuxplan"
   service_plan_tier = "Basic"
